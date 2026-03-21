adb837ff9f76af03ef5a0a6a19b9e9ca397186ca
lmlonewolf <potapenkoaleksandr7@gmail.com>
2026-03-21 23:14:03 +0300

    colored table

--- src/Print.c
+++ src/Print.c
@@ -2,6 +2,7 @@
 
 
 void print_Line() { // Вывод разделения
+	printf("\033[0;033m");
 	putchar('+');
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < (int[]) { 6, 20, 9, 5, 5, 7 }[i]; j++)
@@ -9,6 +10,7 @@ void print_Line() { // Вывод разделения
 		putchar('+');
 	}
 	putchar('\n');
+	printf("\033[0m");
 }
 
 void print_List(const List* const persons) { // Вывод списка студентов ввиде таблицы
@@ -17,11 +19,11 @@ void print_List(const List* const persons) { // Вывод списка студентов ввиде таб
 	else
 		printf("\nСписок из %d студентов:\n", persons->size);
 	print_Line();
-	printf("|%6s|%20s|%9s|%5s|%5s|%s|\n", "ID", "Имя", "Группа", "M1", "M2", "Рейтинг");
+	printf("\033[0;033m|\033[0m%6s\033[0;033m|\033[0m%20s\033[0;033m|\033[0m%9s\033[0;033m|\033[0m%5s\033[0;033m|\033[0m%5s\033[0;033m|\033[0m%s\033[0;033m|\033[0m\n", "ID", "Имя", "Группа", "M1", "M2", "Рейтинг");
 	print_Line();
 	for (int i = 0; i < persons->size; i++) {
 		const Student* p = &persons->arr[i];
-		printf("|%6d|%20s|%9s|%5d|%5d|%7d|\n", p->id, p->name, p->groupstr, p->m1, p->m2, p->rate);
+		printf("\033[0;033m|\033[0m%6d\033[0;033m|\033[0m%20s\033[0;033m|\033[0m%9s\033[0;033m|\033[0m%5d\033[0;033m|\033[0m%5d\033[0;033m|\033[0m%7d\033[0;033m|\033[0m\n", p->id, p->name, p->groupstr, p->m1, p->m2, p->rate);
 		print_Line();
 	}
 	putchar('\n');
@@ -31,8 +33,8 @@ void print_List(const List* const persons) { // Вывод списка студентов ввиде таб
 
 void print_Person(const Student* const person) { // Вывод студента
 	print_Line();
-	printf("|%6s|%20s|%9s|%5s|%5s|%s|\n", "ID", "Имя", "Группа", "M1", "M2", "Рейтинг");
+	printf("\033[0;033m|\033[0m%6s\033[0;033m|\033[0m%20s\033[0;033m|\033[0m%9s\033[0;033m|\033[0m%5s\033[0;033m|\033[0m%5s\033[0;033m|\033[0m%s\033[0;033m|\033[0m\n", "ID", "Имя", "Группа", "M1", "M2", "Рейтинг");
 	print_Line();
-	printf("|%6d|%20s|%9s|%5d|%5d|%7d|\n", person->id, person->name, person->groupstr, person->m1, person->m2, person->rate);
+	printf("\033[0;033m|\033[0m%6d\033[0;033m|\033[0m%20s\033[0;033m|\033[0m%9s\033[0;033m|\033[0m%5d\033[0;033m|\033[0m%5d\033[0;033m|\033[0m%7d\033[0;033m|\033[0m\n", person->id, person->name, person->groupstr, person->m1, person->m2, person->rate);
 	print_Line();
 }
